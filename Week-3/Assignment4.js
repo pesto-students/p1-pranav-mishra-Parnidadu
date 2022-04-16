bcd74e12143a10de9e51c03bcbf5b8fa942746e8
Pranav <pranavmishra559@gmail.com>
2022-04-17 01:25:56 +0530

    Assignment4 is added.

---
+++ Week-3/Assignment4.js
@@ -0,0 +1,17 @@
+function createStack(){
+    return{
+      items:[],
+      push(item){
+        this.items.push(item);
+      },
+      pop(){
+        return this.items.pop();
+      }
+    };
+  }
+  
+  const stack = createStack();
+  stack.push(10);
+  stack.push(5);
+  stack.pop();
+  console.log(stack.items);
